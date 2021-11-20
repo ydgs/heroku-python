67ac9f749d60bc7d97ca31520a8e8e75567e43b5
Sungkur <yash.sungkur@accenture.com>
2021-11-20 13:25:35 +0400

    6 Commit

--- Base.py
+++ Base.py
@@ -8,10 +8,12 @@ random_num = random.randint(1,5)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--start-maximized");
 chrome_options.add_argument('--user-agent=""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36""')
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
